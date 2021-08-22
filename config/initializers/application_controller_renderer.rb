acfbf1b13e0cfb457d8e351b9a19dd2fcece16f3
isseiyoshiyama <issei.1229@gmail.com>
2021-08-22 09:38:15 +0000

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'dae8a07dc64e3717f8e70a46c4ff45fcbf4559614e0b89db9de3a48fa9f9054ab26dd0f198e93a4d3c376d25b81fc29b55b643ee7e6301778244aaefa8931d17'
